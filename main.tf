fb16b6f99b9e6ec822acec62e29f056accca0021
biggiebk <biggiebk@mymadlab.net>
2024-03-29 14:22:53 -0700

    Fix workspace manager source

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 module "tfe_workspace_manager" {
-#  source  = "app.terraform.io/mymadlab/mml_workspace_manager/tfe"
-#  version = "1.0.0"
-	source = "../terraform_tfe_mml_workspace_manager"
+  source  = "app.terraform.io/mymadlab/mml_workspace_manager/tfe"
+  version = "1.0.0"
 
   workspace_name       = var.name
   tfe_org           = var.tfe_org
