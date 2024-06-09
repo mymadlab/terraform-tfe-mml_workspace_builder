6ef35db5ddadd463a10abb1d60b3f6e09be7dd01
Bryce Klimoski <biggiebk@mymadlab.net>
2024-06-09 09:25:26 -0700

    Set workspace terraform version

--- main.tf
+++ main.tf
@@ -1,18 +1,6 @@
-module "tfe_workspace_manager" {
-  source  = "app.terraform.io/mymadlab/mml_workspace_manager/tfe"
-  version = "~> 0.1.0"
-
-  workspace_name    = var.name
-  tfe_org           = var.tfe_org
-  tags              = concat(var.tags, [var.name])
-  vcs_provider_name = var.vcs_provider_name
-  github_org        = var.github_org
-
-}
-
 module "github_repository_manager" {
   source  = "app.terraform.io/mymadlab/mml_repository_manager/github"
-  version = "~> 0.1.0"
+  version = "~>0.1.0"
 
   repo_name   = var.name
   description = var.description
@@ -22,4 +10,17 @@ module "github_repository_manager" {
   product     = "terraform"
 
   depends_on = [ module.tfe_workspace_manager.module ]
+}
+
+module "tfe_workspace_manager" {
+  #source  = "app.terraform.io/mymadlab/mml_workspace_manager/tfe"
+  #version = "~>0.1.0"
+  source  = "../mml_workspace_manager/"
+
+  workspace_name    = var.name
+  tfe_org           = var.tfe_org
+  tags              = concat(var.tags, [var.name])
+  vcs_provider_name = var.vcs_provider_name
+  github_org        = var.github_org
+
 }
